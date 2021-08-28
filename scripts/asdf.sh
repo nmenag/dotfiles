efd4fdc2e6d0263ef2b5c4bce96c4159329f4a9a
nmenag <nmena.garzon@gmail.com>
2021-08-27 22:46:11 -0500

    Add erlang, elixir, ruby, java script instalation

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -8,3 +8,38 @@ source ~/.zshrc
 
 echo "ASDF already installed!!"
 echo $(asdf --version)
+
+echo "Install ruby"
+
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+asdf install ruby latest
+
+asdf global ruby latest
+
+echo "Install Erlang"
+
+asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf install erlang latest
+asdf global erlang latest
+
+export ERL_AFLAGS="-kernel shell_history enabled"
+
+echo -e "\n export ERL_AFLAGS="-kernel shell_history enabled"" >> ~/.zshrc
+
+source ~/.zshrc
+
+echo "Install Elixir"
+
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf install elixir latest
+asdf global elixir latest
+
+echo "Install Java"
+
+asdf plugin-add java
+asdf install java openjdk-14.0.1
+asdf global java openjdk-14.0.1
+$(. ~/.asdf/plugins/java/set-java-home.zsh) # set JAVA_HOME
+
+source ~/.zshrc
