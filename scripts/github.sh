afd67f83401de08e0008c02344942dd66432a523
nmenag <nmena.garzon@gmail.com>
2021-09-03 21:56:22 -0500

    fix up github script

--- scripts/github.sh
+++ scripts/github.sh
@@ -8,5 +8,37 @@ ssh-add ~/.ssh/id_ed25519
 sudo apt-get install xclip
 xclip -selection clipboard < ~/.ssh/id_ed25519.pub
 
-git config --global user.name echo $USERNAME
-git config --global user.email echo $EMAIL
+$(git config --global user.name "$USERNAME")
+$(git config --global user.email "$EMAIL")
+
+cat <<EOT >> ~/.gitconfig
+
+[color]
+        status = auto
+        branch = auto
+        diff = auto
+        interactive = auto
+        ui = auto
+
+[core]
+        editor = nano -w
+
+[color "branch"]
+        current = yellow reverse
+        local = yellow
+        remote = green
+
+[color "diff"]
+        meta = yellow bold
+        frag = magenta bold
+        old = red bold
+        new = green bold
+
+[color "status"]
+	added = yellow
+	changed = green
+	untracked = cyan
+
+[alias]
+        lg = log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+EOT
