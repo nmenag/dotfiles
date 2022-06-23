501cffa8f5e13e5c3df359ae014071f2bc5362d3
nmenag <nmena.garzon@gmail.com>
2022-06-22 22:04:52 -0500

    update gitconfig script

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -45,3 +45,8 @@ cat <<EOT >> ~/.gitconfig
 EOT
 
 echo "finished Git config"
+
+# Create Personal Access Token on GitHub
+# From your GitHub account, go to Settings => Developer Settings => Personal Access Token => Generate New Token (Give your password) => Fillup the form => click Generate token => Copy the generated Token, it will be something like ghp_sFhFsSHhTzMDreGRLjmks4Tzuzgthdvfsrta
+# git config --global credential.helper cache
+# restart the terminal
