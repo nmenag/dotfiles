d9cf87fde554a63a14a8a8bf3dedef077d1738b5
Nicolas Mena <nmena.garzon@gmail.com>
2022-06-30 20:11:34 -0500

    fix libssl1.1 installation

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -6,7 +6,11 @@ wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add 
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
+echo "deb http://security.ubuntu.com/ubuntu impish-security main" | sudo tee /etc/apt/sources.list.d/impish-security.list
+
 sudo apt-get update -y
+
+sudo apt-get install libssl1.1
 
 sudo apt-get install -y mongodb-org
 
