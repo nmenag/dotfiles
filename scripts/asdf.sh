d8d1085b6076216c6105f15d908b3525f715efb0
Nicolas Mena <nmena.garzon@gmail.com>
2022-11-19 20:53:07 -0500

    Add python installation

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -28,4 +28,10 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs latest
 asdf global nodejs latest
 
+echo "Install python"
+
+asdf plugin-add python
+asdf install python latest
+asdf global python latest
+
 source ~/.zshrc
