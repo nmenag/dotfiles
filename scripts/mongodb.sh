858951b2bfe67954d2e64bfa4f79faf8c76d9843
nmenag <nmena.garzon@gmail.com>
2023-01-12 23:01:02 -0500

    fix up mongo script

--- scripts/mongodb.sh
+++ scripts/mongodb.sh
@@ -10,7 +10,9 @@ echo "deb http://security.ubuntu.com/ubuntu impish-security main" | sudo tee /et
 
 sudo apt-get update -y
 
-sudo apt-get install libssl1.1
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1\~18.04.20_amd64.deb
+
+sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
 
 sudo apt-get install -y mongodb-org
 
@@ -19,3 +21,18 @@ sudo systemctl start mongod
 # if failed the start mongoDB
 
 # sudo systemctl daemon-reload
+
+
+# check mongo status 
+
+sudo systemctl status mongod
+
+#  mongod.service - MongoDB Database Server
+#      Loaded: loaded (/lib/systemd/system/mongod.service; disabled; vendor preset: enabled)
+#      Active: active (running) since Thu 2023-01-12 22:55:45 -05; 16s ago
+#        Docs: https://docs.mongodb.org/manual
+#    Main PID: 14875 (mongod)
+#      Memory: 61.1M
+#         CPU: 226ms
+#      CGroup: /system.slice/mongod.service
+#              └─14875 /usr/bin/mongod --config /etc/mongod.conf
