9348dc7ee85683ada63a47286372b52dd692889e
Nicolas Mena <nmena.garzon@gmail.com>
2024-03-15 20:30:48 -0500

    Update postgres.sh

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-echo 'Updating System!!'
-sudo apt-get update -y && sudo apt-get upgrade -y
-
 echo 'Installing Postgres'
 
-sudo apt-get install wget ca-certificates -y
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ 'lsb_release -cs'-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+echo 'Import the repository signing key'
+$ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
 
-sudo apt-get update
+echo 'update!!!!'
+$ sudo apt update
 
 sudo apt-get install postgresql postgresql-contrib
 
